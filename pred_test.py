190b060c04316c5d04f023a6f35c126df4ccf073
stwater20 <54sakuramiku@gmail.com>
2019-12-16 12:55:41 +0800

    sencond upload

--- pred_test.py
+++ pred_test.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 
 def main():
     categories = ["Cat","Dog","Squirrel"]
-    model = tf.keras.models.load_model("64x3-CNN.h5")
-    x = sys.argv[0]
+    model = tf.keras.models.load_model("64x3-CNN.model")
+    x = sys.argv[1]
     testing = repr(x) # example: C:\Users\box88\Desktop\ai\1.jpg
     prediction = model.predict([prepare(testing)])
     maxindex  = np.argmax(prediction)
@@ -15,3 +15,4 @@ def main():
 if __name__ == "__main__":
     main()
 
+main()
